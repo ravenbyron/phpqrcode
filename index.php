d67f069a216b020a129aba17d44d3003862bfe19
Byron Albert <byron@raventools.com>
2014-07-30 14:27:54 -0500

    fix up inde

--- index.php
+++ index.php
@@ -24,20 +24,7 @@
     
     echo "<h1>PHP QR Code</h1><hr/>";
     
-    //set it to writable location, a place for temp generated PNG files
-    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
-    
-    //html PNG location prefix
-    $PNG_WEB_DIR = 'temp/';
-
     include "phpQRCode/qrlib.php";    
-    
-    //ofcourse we need rights to create temp dir
-    if (!file_exists($PNG_TEMP_DIR))
-        mkdir($PNG_TEMP_DIR);
-    
-    
-    $filename = $PNG_TEMP_DIR.'test.png';
     
     //processing form input
     //remember to sanitize user input in real-life solution !!!
